e022700b7ffd03066392e83977e525973e6ae684
David Pietrzak <dtpietrzak@gmail.com>
2020-12-30 22:19:38 -0600

    Create dontshoot-conf.js
    Moved config to it's own file

---
+++ dontshoot-conf.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SERVER_ADDRESS = exports._0x3f07OP = void 0;
+exports._0x3f07OP = {
+    "_0x5d6Xa4": "asdfasdf",
+    "_0x4d1E54": "asdfasdf",
+    "_0x2AF7de": "asdfasdf",
+    "_0x2a2386": "asdfasdf",
+    "_0x200a32": "asdfasdf",
+    "_0x325G24": "asdfasdfasdfasdfasdfasdfasdfasdf",
+    "_0x500T23": "asdfasdf",
+    "_0x29G808": "asdfasdfasdfasdfasdfasdfasdfasdf",
+    "_0x857S78": "asdfasdf",
+    "_0x920G46": "asdfasdf",
+};
+exports.SERVER_ADDRESS = 'your_address_or_ip';
